153c707c477e1a06b23d9a8d6367a9fa9c255f42
Roman E <rk2555@yandex.ru>
2025-12-04 16:55:47 +0300

    файлам присвоены переменные

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import csv
 import re
 
+INPUT_FILE_NAME = "phonebook_raw.csv"
+OUTPUT_FILE_NAME = "phonebook.csv"
+
 try:
-    with open("phonebook_raw.csv", encoding="utf-8") as f:
+    with open(INPUT_FILE_NAME, encoding="utf-8") as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
 except FileNotFoundError as e:
-    print(f"Ошибка: файл 'phonebook_raw.csv' не найден. {e}")
+    print(f"Ошибка: файл '{INPUT_FILE_NAME}' не найден. {e}")
     exit(1)
 except Exception as e:
     print(f"Ошибка при чтении файла: {e}")
     exit(1)
 
 
-def format_phone(phone):
+def format_phone_number(phone_number):
     pattern = (
         r"[\s\-\(]*(\+?7|8)?[\s\-\(]*(\d{3})[\s\-\)]*(\d{3})"
         r"[\s\-\)]*(\d{2})[\s\-\)]*(\d{2})[\s\-\(]*(доб\.?)?"
         r"[\.\s\-]?\s*(\d+)?[\s\)]*"
     )
     repl = r"+7(\2)\3-\4-\5"
-    if "доб." in phone:
+    if "доб." in phone_number:
         repl += r" доб.\7"
-    formatted = re.sub(pattern, repl, phone)
+    formatted = re.sub(pattern, repl, phone_number)
     return formatted.strip()
 
 
@@ -42,7 +45,7 @@ for row in data:
     surname = full_name[2] if len(full_name) > 2 else ""
     organization = row[3] if len(row) > 3 else ""
     position = row[4] if len(row) > 4 else ""
-    phone = format_phone(row[5]) if len(row) > 5 else ""
+    phone_number = format_phone_number(row[5]) if len(row) > 5 else ""
     email = row[6] if len(row) > 6 else ""
 
     key = (lastname, firstname)
@@ -54,12 +57,12 @@ for row in data:
             surname,
             organization,
             position,
-            phone,
+            phone_number,
             email,
         ]
     else:
         existing = contacts_dict[key]  # берём сохранённую запись
-        for i, value in enumerate([surname, organization, position, phone, email]):
+        for i, value in enumerate([surname, organization, position, phone_number, email]):
             if value and not existing[i + 2]:
                 existing[i + 2] = value
 
@@ -71,7 +74,7 @@ final_contacts_list.sort(key=lambda x: x[0])  # Сортировка по алф
 # pprint(final_contacts_list)
 
 try:
-    with open("phonebook.csv", "w", encoding="utf-8", newline="") as f:
+    with open(OUTPUT_FILE_NAME, "w", encoding="utf-8", newline="") as f:
         datawriter = csv.writer(f, delimiter=",")
         datawriter.writerows(final_contacts_list)
 except Exception as e:
